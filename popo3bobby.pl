3b1944a03918a84b390fd9fafb6629e397e5b7f6
Prince of Archives <rich.tijerina@gmail.com>
2018-03-06 20:42:32 -0800

    Update popo3bobby.pl

--- popo3bobby.pl
+++ popo3bobby.pl
@@ -7,4 +7,18 @@ use strict; use warnings;
 
 my %list;
 my $list = '~/immute_LIST';
-open(
+open(my $fh, '<', $list);
+my @files = readline $fh; 
+close $fh; chomp @files;
+
+for (@files)
+{
+  my @i = split(' ', $_);
+  $list{$i[0]}=$i[1];
+}
+
+for (keys %list}
+{
+  my $isha = sha($_);
+  print FAILfh "FAIL $_ $isa != $list{$_}\n"  unless ($isha eq $list{$_}); 
+}
