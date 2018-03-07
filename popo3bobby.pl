eb3d3599bc1cc00228753f738109c1edd9761481
Prince of Archives <rich.tijerina@gmail.com>
2018-03-06 20:37:29 -0800

    Create popo3bobby.pl

---
+++ popo3bobby.pl
@@ -0,0 +1,10 @@
+#!/usr/local/bin/perl
+use strict; use warnings;
+#########################
+# bobby - popo DEMON
+#########################
+# %list{$file} = $sha
+
+my %list;
+my $list = '~/immute_LIST';
+open(
